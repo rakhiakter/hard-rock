defff111435a44f5ca6b94c867e75b68f81e5e15
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-10-28 17:40:44 +0600

    all added

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
+# Hard-rock
+## Features
+1. Hard-rock is a Lyrics Searching website.
+2. A user easily can find any lyrics by this site.
+3. If users search any word then they get suggestions of lyrics of this particular word.
+4. Users can arrange results based on individual song title or album.
+5. It comes with a clean and straightforward search system.
+6. It provides segments of lyrics in search results to help you identify the song before heading to the full lyrics’ page.
+7. Outstanding performance when searching current hits.
+
+# Technology
+HTML5, CSS3, JavaScript, JSON etc.
+
+# Tools
+VS Code, Git, Chrome Dev Tool, Figma.
+
+# LiveSite Link
+https://rakhiakter.github.io/hard-rock/
+
+# Resume
+
+# ScreenShots
+![Home](home.png)
+![Title](title.png)
+![Lyrics](lyrics.png)
+
+
 # hard-rock-solution
 api link: https://api.lyrics.ovh/suggest/summer
--- search.js
+++ search.js
@@ -15,20 +15,56 @@ form.addEventListener('submit', e=> {
 
 })
 async function searchSong(searchValue){
-    const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`);
+   fetch(`${apiURL}/suggest/${searchValue}`).then(response=>response.json())
+   .then(data=>console.log(data))
+       const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`);
 
     const data = await searchResult.json();
-    console.log(data);
     showData(data);
 }
-// function showData(data){
-//   result.innerHTML = `
-//   <ul class = "song-list">
-//   ${data.data.map(song=> `<li>
-//   `)
-// }
-//   `
-    
-//   )};
+function showData(data){
+    console.log(data.data);
+    result.innerHTML = `
+    <ul class="song-list">
+    ${data.data.map(song=> `<li>
+                            <div>
+                                <strong>
+                                ${song.artist.name}
+                                </strong> -${song.title}
+                                </div>
+                                <span data.artist="${song.artist.name}" data-songtitle="${song.title}"> 
+                                get lyrics 
+                                </span>
+                                </li>
+    `).join('')
+    }
+    </ul>
+    `
+}
+result.addEventListener('click', e=>{
+    const clickedElement = e.target;
+
+    if(clickedElement.tagName === 'SPAN'){
+    const artist = clickedElement.getAttribute('data-artist');
+    const songTitle = clickedElement.getAttribute('data-songtitle');
+    getLyrics(artist, songTitle)
+    }
+})
+
+async function getLyrics(artist, songTitle){
+const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+const data = await res.json();
+console.log(data);
+const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+result.innerHTML = ` <h2> <strong>
+                            ${artist}
+                            </strong> -${songTitle}
+                            </h2>
+                            <p> ${lyrics}</p>
+
+
+
+`
+}
     
  
